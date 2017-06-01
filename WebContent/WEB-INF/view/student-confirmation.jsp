b34bcdd19dfcf8d97b25aecc7b8900c3b3834aab
Yinan Zhu <zynsteve@tamu.edu>
2017-06-01 15:13:43 -0400

    programming languages and operating systems

--- WebContent/WEB-INF/view/student-confirmation.jsp
+++ WebContent/WEB-INF/view/student-confirmation.jsp
@@ -16,6 +16,23 @@ The student is confirmed: ${student.firstName} ${student.lastName}
 
 Country: ${student.country}
 
+<br><br>
+
+Favorite Language: ${student.favoriteLanguage}
+
+<br><br>
+
+Operation Systems:
+
+<ul>
+	<c:forEach var="temp" items="${student.operatingSystems}">
+	
+		<li> ${temp} </li>
+		
+	</c:forEach>
+</ul>
+
+
 </body>
 
 </html>
--- src/com/luv2code/springdemo/mvc/Student.java
+++ src/com/luv2code/springdemo/mvc/Student.java
@@ -11,6 +11,10 @@ public class Student {
 	
 	private LinkedHashMap<String, String> countryOptions;
 	
+	private String favoriteLanguage;
+	
+	private String[] operatingSystems;
+	
 	public Student() {
 		
 		// populate country options: used ISO country code
@@ -51,6 +55,22 @@ public class Student {
 
 	public LinkedHashMap<String, String> getCountryOptions() {
 		return countryOptions;
+	}
+
+	public String getFavoriteLanguage() {
+		return favoriteLanguage;
+	}
+
+	public void setFavoriteLanguage(String favoriteLanguage) {
+		this.favoriteLanguage = favoriteLanguage;
+	}
+
+	public String[] getOperatingSystems() {
+		return operatingSystems;
+	}
+
+	public void setOperatingSystems(String[] operatingSystems) {
+		this.operatingSystems = operatingSystems;
 	}	
 	
 }
